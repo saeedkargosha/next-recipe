e113f0a65a50ee54ea6d6a50a1eb9b70ae8a357b
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-19 19:04:12 +0330

    Change styles for search input

--- src/uikit/SearchInput/index.tsx
+++ src/uikit/SearchInput/index.tsx
@@ -8,16 +8,16 @@ export interface InputProps
 
 export function SearchInput({ className, id, ...rest }: InputProps) {
   const inputStyle = clsx(
-    'block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+    'block w-full p-4 pl-10 text-sm text-neutral-900 rounded-full bg-gray-100 outline-none placeholder:text-neutral-600',
     className
   )
   return (
     <div>
       <div className='relative'>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-          <Search className='w-4 h-4 text-gray-500 dark:text-gray-400' />
+          <Search className='w-4 h-4 text-gray-500 ' />
         </div>
-        <input type='search' className={inputStyle} {...rest} />
+        <input type='search' className={inputStyle + ' '} {...rest} />
       </div>
     </div>
   )

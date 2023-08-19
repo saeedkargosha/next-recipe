f5ac755b279ae07aa537395b4d0a51a94c37c0a9
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-19 12:13:53 +0330

    Add localization to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
+import { useTranslations } from 'next-intl'
+
 export function Header() {
+  const t = useTranslations()
   return (
     <header className='sticky top-0 bg-white px-4 py-8 z-50'>
-      <h1 className='text-3xl font-bold text-slate-700'>{'Hava'}</h1>
-      <div className='text-slate-500'>{'Appetite for life'}</div>
+      <h1 className='text-3xl font-bold text-slate-700'>{t('Hava')}</h1>
+      <div className='text-slate-500'>{t('Appetite for life')}</div>
     </header>
   )
 }

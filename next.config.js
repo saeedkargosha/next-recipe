1ac6ae0acd780c4f591757c93e720a9d453fbac8
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-19 12:13:37 +0330

    Setup localization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withNextIntl = require('next-intl/plugin')
+
+const withPWA = withNextIntl('./src/i18n/index.ts')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,10 @@
 import { Header } from '@/components/header'
 
-export default function Home() {
+type HomeProps = {
+  params: { lng: string }
+}
+
+export default function Home({ params: { lng } }: HomeProps) {
   return (
     <main className='container mx-auto px-4 py-8 min-h-screen max-w-5xl'>
       <Header />

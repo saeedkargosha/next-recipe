35a813ed99cf33aefaae492cddce6d5084505eac
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-19 11:38:05 +0330

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className='sticky top-0 bg-white px-4 py-8 z-50'>
+      <h1 className='text-3xl font-bold text-slate-700'>{'Hava'}</h1>
+      <div className='text-slate-500'>{'Appetite for life'}</div>
+    </header>
+  )
+}
